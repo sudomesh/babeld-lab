254c39af516ca8680be5c4139d7c4ebd86b8c1e5
Jehan <jehan.tremback@gmail.com>
2017-04-19 22:00:48 -0700

    change to netlab

--- network-lab.sh
+++ network-lab.sh
@@ -4,12 +4,14 @@
 ip -all netns delete
 
 # add namespaces
+echo "adding nodes"
 for node in $(jq '.nodes | keys[]' < "$1")
 do
-  ip netns add "ns-${node:1:-1}"
+  ip netns add "netlab-${node:1:-1}"
 done
 
 # iterate over edges array
+echo "adding edges"
 length=$(jq '.edges | length' < $1)
 for ((i=0; i<$length; i++)); do
 
@@ -23,28 +25,29 @@ for ((i=0; i<$length; i++)); do
   ip link add "veth-$A-$B" type veth peer name "veth-$B-$A"
 
   # assign each side of the veth to one of the nodes namespaces
-  ip link set "veth-$A-$B" netns "ns-$A"
-  ip link set "veth-$B-$A" netns "ns-$B"
+  ip link set "veth-$A-$B" netns "netlab-$A"
+  ip link set "veth-$B-$A" netns "netlab-$B"
 
   # add ip addresses on each side
   ipA=$(jq '.nodes["'$A'"].ip' < $1)
   ipB=$(jq '.nodes["'$B'"].ip' < $1)
-  ip netns exec "ns-$A" ip addr add ${ipA:1:-1} dev "veth-$A-$B"
-  ip netns exec "ns-$B" ip addr add ${ipB:1:-1} dev "veth-$B-$A"
+  ip netns exec "netlab-$A" ip addr add ${ipA:1:-1} dev "veth-$A-$B"
+  ip netns exec "netlab-$B" ip addr add ${ipB:1:-1} dev "veth-$B-$A"
 
   # bring the interfaces up
-  ip netns exec "ns-$A" ip link set dev "veth-$A-$B" up
-  ip netns exec "ns-$B" ip link set dev "veth-$B-$A" up
+  ip netns exec "netlab-$A" ip link set dev "veth-$A-$B" up
+  ip netns exec "netlab-$B" ip link set dev "veth-$B-$A" up
 
   # add some connection quality issues
   AtoB=$(jq '.edges['$i']["->"]' < $1)
   BtoA=$(jq '.edges['$i']["<-"]' < $1)
 
-  ip netns exec "ns-$A" tc qdisc add dev "veth-$A-$B" root netem ${AtoB:1:-1}
-  ip netns exec "ns-$B" tc qdisc add dev "veth-$B-$A" root netem ${BtoA:1:-1}
+  ip netns exec "netlab-$A" tc qdisc add dev "veth-$A-$B" root netem ${AtoB:1:-1}
+  ip netns exec "netlab-$B" tc qdisc add dev "veth-$B-$A" root netem ${BtoA:1:-1}
 done
 
 # run startup scripts
+echo "running startup scripts"
 for node in $(jq '.nodes | keys[]' < "$1")
 do
   script=$(jq '.nodes['$node'].startup' < $1)
